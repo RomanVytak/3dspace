bc1d5698440753b233e3487398832f021b3f7bb7
RomanVytak <roman.vytak@gmail.com>
2015-05-02 03:29:20 +0300

    new

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -1,16 +1,3 @@
-// identify browser -------------------------------------------------- //
-
-if(navigator.userAgent.match(/(iPad|iPhone|iPod)/g)){
-    document.documentElement.classList.add('ios')
-};
-if(navigator.userAgent.match(/Android/i)) { 
-    document.documentElement.classList.add('android') 
-};
-if (document.all && document.querySelector && !document.addEventListener) {
-    document.documentElement.classList.add('ie8')
-};
-
-
 
 // fadeIn page (load or DOMContentLoaded); -------------------------------------------------- //
 
@@ -19,7 +6,49 @@ window.addEventListener && window.addEventListener('load',function(){
 });
 
 
-
+(function(){
+    var lengthSlider = $('.project .wrapper-items > div').length
+    // active for first element
+    $('.project .wrapper-items > div:first-child').addClass('active')
+    // click item page change image
+    $('.project .wrapper-items > div').on('click', function(){
+        $('.project .wrapper-items > div').removeClass('active')
+        $(this).addClass('active')
+        var myImage = $(this).find('span').attr('data-url')
+        $('.project .big-image-wrapper .big-image').css('background', myImage)
+    });
+    // click right
+    $('.project .big-image-wrapper .slide-right').on('click', function(){
+        var indexItem = $('.project .wrapper-items > div.active').index() + 1;
+        $('.project .controls').removeClass('show-left-button')
+        if(indexItem < lengthSlider) {
+            $('.project .wrapper-items > div.active').removeClass('active').next('div').addClass('active')
+        } else {
+            $('.project .controls').addClass('show-right-button')
+        }
+    })
+    // click left
+    $('.project .big-image-wrapper .slide-left').on('click', function(){
+        var indexItem = $('.project .wrapper-items > div.active').index() + 1;
+        $('.project .controls').removeClass('show-right-button')
+        if(indexItem > 1) {
+            $('.project .wrapper-items > div.active').removeClass('active').prev('div').addClass('active')
+        } else {
+            $('.project .controls').addClass('show-left-button')
+        }
+    })
+    // change image after click right / left
+    $('.project .controls > span').on('click', function(){
+        var myImage = $('.project .wrapper-items > div.active').find('span').attr('data-url')
+        $('.project .big-image-wrapper .big-image').css('background', myImage)
+    })
+    
+    //
+    
+    
+    
+    
+})();
 
 
 
